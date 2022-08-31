37409765682693b684d81badccaf97679b4efb8d
Nik Zandian <nik@quietrocket.com>
2022-08-31 19:37:40 +0200

    Adapted config to Pearl

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'e26d4e0be54d405ca42639406d848b60',
+  rootNotionPageId: '936c53b7361d4b78bc4827262b97519a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Nik Zandian\'s Slice of the Internet',
-  domain: 'quietrocket.com',
-  author: 'Nik Zandian',
+  name: 'Oyster in Montréal',
+  domain: 'pearl.quietrocket.com',
+  author: 'Oyster Pearl',
 
   // open graph metadata (optional)
-  description: 'My slice of the internet',
+  description: 'Oyster in Montréal',
 
   // social usernames (optional)
-  twitter: null,
-  github: 'quietrocket',
-  linkedin: 'nzandian',
+  // twitter: null,
+  // github: 'quietrocket',
+  // linkedin: 'nzandian',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
